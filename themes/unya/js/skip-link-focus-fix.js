74dd2cb63227034858465ba5fa1b3f308df0bf96
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-12 14:04:20 -0700

    initial commit

---
+++ themes/unya/js/skip-link-focus-fix.js
@@ -0,0 +1,33 @@
+/**
+ * skip-link-focus-fix.js
+ *
+ * Helps with accessibility for keyboard only users.
+ *
+ * Learn more: https://github.com/Automattic/RED Starter Theme/pull/136
+ */
+(function () {
+	var isWebkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
+		isOpera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
+		isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+
+	if ((isWebkit || isOpera || isIE) && document.getElementById && window.addEventListener) {
+		window.addEventListener('hashchange', function () {
+			var id = location.hash.substring(1),
+				element;
+
+			if (!(/^[A-z0-9_-]+$/.test(id))) {
+				return;
+			}
+
+			element = document.getElementById(id);
+
+			if (element) {
+				if (!(/^(?:a|select|input|button|textarea)$/i.test(element.tagName))) {
+					element.tabIndex = -1;
+				}
+
+				element.focus();
+			}
+		}, false);
+	}
+})();
