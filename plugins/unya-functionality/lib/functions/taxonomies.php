63c39068276c5d61b2fed3b99a5161d717707aa6
Jennifer Ta <jenniferta90@gmail.com>
2017-06-13 15:16:53 -0700

    created taxonomy opportunity type

--- plugins/unya-functionality/lib/functions/taxonomies.php
+++ plugins/unya-functionality/lib/functions/taxonomies.php
@@ -44,3 +44,43 @@ function register_program_type_taxonomy() {
 }
 add_action( 'init', 'register_program_type_taxonomy', 0 );
 
+
+// Register Custom Taxonomy
+function opportunities_type_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'Opportunity Types',
+		'singular_name'              => 'Opportunity Type',
+		'menu_name'                  => 'Opportunity Type',
+		'all_items'                  => 'All Opportunity Types',
+		'parent_item'                => 'Parent Opportunity Type',
+		'parent_item_colon'          => 'Parent Opportunity Type:',
+		'new_item_name'              => 'New Opportunity_Type Name',
+		'add_new_item'               => 'Add New OpportunityType',
+		'edit_item'                  => 'Edit Opportunity Type',
+		'update_item'                => 'Update Opportunity Type',
+		'view_item'                  => 'View Opportunity Type',
+		'separate_items_with_commas' => 'Separate Opportunity Types with commas',
+		'add_or_remove_items'        => 'Add or remove Opportunity Type',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Opportunity Types',
+		'search_items'               => 'Search Opportunity Types',
+		'not_found'                  => 'Opportunity Types Not Found',
+		'no_terms'                   => 'No Opportunity Types',
+		'items_list'                 => 'Opportunity Types list',
+		'items_list_navigation'      => 'Opportunity Type list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => false,
+	);
+	register_taxonomy( 'opportunity type', array( 'opportunities' ), $args );
+
+}
+add_action( 'init', 'opportunities_type_taxonomy', 0 );
