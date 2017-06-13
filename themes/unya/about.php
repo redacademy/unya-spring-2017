bb2547c2bebd587b7cf1c1a3f345f31ac6cf7d0f
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-13 15:04:19 -0700

    added about page template

---
+++ themes/unya/about.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the about page.
+ * Template Name: About Page
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
