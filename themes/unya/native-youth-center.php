753967a7bf45a3ac3edddb8616e8fa2ebb6ae208
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-13 15:25:24 -0700

    added native youth center page template

---
+++ themes/unya/native-youth-center.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the native youth center page.
+ * Template Name: Native Youth Center Page
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
