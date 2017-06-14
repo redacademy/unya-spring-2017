9ef57b953b34acbf5395c198a47bc206e95369d4
Jennifer Ta <jenniferta90@gmail.com>
2017-06-13 18:48:20 -0700

    added markup to footer

--- themes/unya/footer.php
+++ themes/unya/footer.php
@@ -11,6 +11,27 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
+				<div class="email-list">
+					<h2> Sign up for our updates!</h2>
+					<p> Yes, please add me to the mailing list.</p>
+					<p> sign up</p>
+				</div>
+
+				<div class="social-media">
+					<h2>Get Social </h2>
+					<span class="fa fa-facebook-square fa-3x" aria-hidden="true"></span>
+					<span class="fa fa-twitter-square fa-3x" aria-hidden="true"></span>
+					<span class="fa fa-instagram fa-3x" aria-hidden="true"></span>
+				</div>
+
+				<div class="contact-info">
+					<h2>Contact Us </h2>
+					<p>Urban Native Youth Association</p>
+					<p>1630 Hastings Street East </p>
+					<p>604-254-7732</p>
+					<p>info@unya.bc.ca</p>
+				</div>
+
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
--- themes/unya/page-get-involved.php
+++ themes/unya/page-get-involved.php
@@ -10,6 +10,32 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+	<section class="donation">
+		<h1>Donate</h1>
+		<div class="donation-registration">
+			<p>UNYA is a not-for-profit society registered with the Province and a
+			charitable organization 129836292 RR0001 registered with the Government of Canada.</p>
+		</div>
+		<div class="donate-button">
+			<h1> MAKE YOUR DONATION ONLINE TODAY </h1>
+			<a href="https://www.canadahelps.org/en/charities/urban-native-youth-association/">
+			<h1> DONATE TODAY </h1>
+			</a>
+		</div>
+	</section>
+	<section class="employment opportunities">
+		<p><span class="important-note">Important Note:</span> UNYA accepts complete applications for advertised positions only.
+		You must state clearly on your cover letter which position you are applying for. All applications must be sent in electronic
+		form, to the email address specified on the posting. Hard copies and unsolicited applications/resumes will not be reviewed,
+		but immediately destroyed.</p>
+	</section>
+	<section class="volunteer opportunities">
+		<h1> Volunteer Opportunities</h1>
+	</section>
+	<section class="opportunity-cta">
+		<p>please email INFO@UNYA.BC.CA for additional job opportunities, or to be added to our email list.</p>
+	</section>
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
