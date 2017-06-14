8a0219cb4a376fd47f00d70eeeb9744d3d7048fb
Jennifer Ta <jenniferta90@gmail.com>
2017-06-14 09:58:02 -0700

    removed just another wordpress site text on header

--- themes/unya/header.php
+++ themes/unya/header.php
@@ -23,7 +23,6 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
