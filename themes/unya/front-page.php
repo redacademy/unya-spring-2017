be4ff34fb211b7147ed1ec4175d23d6335a2e7ce
Jennifer Ta <jenniferta90@gmail.com>
2017-06-13 19:17:05 -0700

    added markup to front-page.php

---
+++ themes/unya/front-page.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+    <section class="about">
+      <div class="home-container">
+        <h1>unya: where youth lead </h1>
+        <p>The talented youth at UNYA are making an impact in our community. Learn about their successes.</p>
+        <h2>learn more</h2>
+      </div>
+    </section>
+    <section class="nyc">
+      <div class="home-container">
+        <h1>Join us in making history</h1>
+        <p>The Native Youth Center capital campaign will help us build a new hub for
+        Vancouver's indigenous youth. Learn how you can contribute.</p>
+        <h1>Learn More</h1>
+      </div>
+    </section>
+    <section class="programs">
+      <div class="home-container">
+        <h1>Programs at UNYA</h1>
+        <p>What's your thing? At UNYA, there's something for everyone. Connect with us to
+        learn more about the programs that UNYA can offer to you.</p>
+        <h1>Learn More</h1>
+      </div>
+    </section>
+    <section class="donation-cta">
+      <div class="donation-container">
+        <h1>Make your donation online today</h1>
+        <h1>Donate Today </h1>
+      </div>
+    </section>
+    <section class="instagram-images">
+      <div class="home-container">
+        <h1>Get social with #UNYAwhat</h1>
+        <!--<?php get_post(); ?>-->
+      </div>
+    </section>
+<?php get_footer(); ?>
