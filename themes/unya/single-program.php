077372b3a36b71c877a3f3d661528130d981737f
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-14 09:13:49 -0700

    create single program page

--- themes/unya/single-program.php
+++ themes/unya/single-program.php
@@ -16,13 +16,6 @@ get_header(); ?>
 
 			<?php the_post_navigation(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
 		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
