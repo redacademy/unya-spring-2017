5c3261e1ed4555fd7bb44ae0f899355b77d648a5
Cory <Coryjh85@gmail.com>
2017-06-15 09:06:18 -0700

    news page layout

--- themes/unya/archive-news.php
+++ themes/unya/archive-news.php
@@ -10,30 +10,30 @@ $args= array(
      'posts_per_page'=> 8
  );
 
+ $news = get_posts($args);
+
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+      <header class="page-header">
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
-		 <div class=“content-wrapper”>
-			<?php foreach ( $news as $post ) : setup_postdata( $post ); ?>
-				<h3><?php the_title(); ?></h3>
-				<img class="news-image" src="<?php echo CFS()->get( 'article_photo' ); ?>">			
-				<p><?php echo CFS()->get( 'article_text' ); ?></p>				
-				<p><?php echo CFS()->get( 'article_date' ); ?></p>		
-				<p><?php echo CFS()->get( 'article_url' ); ?></p>						
-		  	<?php endforeach; wp_reset_postdata(); ?> 
-      </div>
+		 		<div class=“content-wrapper”>
+		 			<ul class="news-articles">
+					 	<?php foreach ( $news as $post ) : setup_postdata( $post ); ?>
+						 	<li class="news-single">
+								<h3><?php the_title();?>
+								</h3>
+								<img class="news-image" src="<?php echo CFS()->get( 'article_photo' ); ?>">			
+								<p><?php echo CFS()->get( 'article_text' ); ?></p>				
+								<p><?php echo CFS()->get( 'article_date' ); ?></p>		
+								<p><?php echo CFS()->get( 'article_url' ); ?></p>						
+		  				</li>
+						<?php endforeach; wp_reset_postdata(); ?> 
+					</ul>
+			  </div>
 
 
 
--- themes/unya/page-impact.php
+++ themes/unya/page-impact.php
@@ -16,13 +16,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
      
+	 <?php /* Start the Loop */ ?>
 		 <div class=“content-wrapper”>
-			<?php foreach ( $success_story as $post ) : setup_postdata( $post ); ?>
-				<h3><?php the_title(); ?></h3>
-				<img class="success-image" src="<?php echo CFS()->get( 'photo' ); ?>">			
-				<p><?php echo CFS()->get( 'first_half_of_story' ); ?></p>				
-		  	<?php endforeach; wp_reset_postdata(); ?> 
-      </div>
+		 	 <?php foreach ( $success_story as $post ) : setup_postdata( $post ); ?>
+				 <h3><?php the_title(); ?></h3>
+				 <img class="success-image" src="<?php echo CFS()->get( 'photo' ); ?>">			
+				 <p><?php echo CFS()->get( 'first_half_of_story' ); ?></p>				
+		   <?php endforeach; wp_reset_postdata(); ?> 
+     </div>
 
     <h2>Testimonials</h2>
 		<p>What do youth say about us?</p>
