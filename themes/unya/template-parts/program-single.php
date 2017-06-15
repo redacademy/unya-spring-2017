d8c5fc76b566fdda100a37a184adba57407ba265
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-14 22:50:36 -0700

    add loop to single program to limit images displayed

--- themes/unya/template-parts/program-single.php
+++ themes/unya/template-parts/program-single.php
@@ -11,9 +11,33 @@
 		<?php the_title( '<h2 class="program-title">', '</h2>' ); ?>
 
 	<div class="program-content content-wrapper">
-		<p><?php echo esc_html( CFS()->get( 'program_details' ) ); ?></p>
+		<p><?php echo wpautop( esc_html( CFS()->get( 'program_details' ) ) ); ?></p>
 		<a href="<?php echo esc_html( CFS()->get( 'program_brochure' ) ); ?>"><i class="fa fa-file-o" aria-hidden="true"></i> <?php the_title(); ?> Program Brochure</a>
-		
+		<h3>Program Activities</h3>
+		<ul>
+		<?php	
+			$activities = CFS()->get( 'program_activities' );
+			foreach ( $activities as $activity ) : ?>
+			<li><?php echo $activity['program_activity']; ?></li>
+			<?php endforeach; ?>
+		</ul>
+		<?php $images = CFS()->get( 'program_image_slider' ); ?>
+			<?php 
+				$i = 0;
+				foreach ( $images as $image ) : 
+					if ($i == 3) break;
+			?>
+					<div><img src="<?php echo $image['image']; ?>"></div>
+					<?php $i++;
+				endforeach; ?>
+		<h3>How To Get Involved</h3>
+		<p><?php echo wpautop( esc_html( CFS()->get( 'get_involved' ) ) ); ?></p>
+		<ul class="image-slider">
+			<?php foreach ( $images as $image ) : ?>
+				<li><img src="<?php echo $image['image']; ?>"></li>
+			<?php endforeach; ?>
+		</ul><!--image slider-->
+}
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
