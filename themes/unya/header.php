3aeec8a56767a851dc40da00ae1d8540ca18bbe3
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-15 15:46:04 -0700

    moved menu toggleoutside main-navigation div

--- themes/unya/header.php
+++ themes/unya/header.php
@@ -24,8 +24,8 @@
 				<div class="site-branding">
 					<div class="logo"><a href=<?php echo esc_url( home_url()) ?> rel="home"></a></div>
 				</div><!-- .site-branding -->
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 				<a href="#" class="donate">Donate</a>
