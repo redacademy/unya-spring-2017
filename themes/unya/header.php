764de1583dbf4e7a01028585d8f8aaef8b24e4b5
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-15 10:49:36 -0700

    fix indentation

--- themes/unya/header.php
+++ themes/unya/header.php
@@ -29,7 +29,7 @@
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
-				<button class="donate">Donate</button>
+					<button class="donate">Donate</button>
 				</div>
 			</header><!-- #masthead -->
 
