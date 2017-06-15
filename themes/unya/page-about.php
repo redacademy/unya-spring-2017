1dd7a3b734ab3226ad3a9d49e6c16592fd79d5da
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-15 15:48:09 -0700

    add css to content on about page

--- themes/unya/page-about.php
+++ themes/unya/page-about.php
@@ -19,26 +19,34 @@ get_header(); ?>
 			<h1>About UNYA</h1>
 		</div>
 		<section class="vision" id="vision">
+				<header class="section-heading">
+					<h2>Vision &amp; Mission</h2>
+				</header>
 			<div class="content-wrapper">
-				<h2>Vision</h2>
+				<h3>Vision</h3>
 				<p><?php echo esc_html( CFS()->get( 'vision' ) ); ?></p>
 			</div>
 		</section>
 		<section class="mission" id="mission">
 			<div class="content-wrapper">
-				<h2>Mission</h2>
+				<h3>Mission</h3>
 				<p><?php echo esc_html( CFS()->get( 'mission' ) ); ?></p>
 			</div>
 		</section>
 		<section class="brief-history" id="brief-history">
+				<header class="section-heading">
+					<h2>History</h2>
+				</header>
 			<div class="content-wrapper">
-				<h2>Brief History</h2>
+				<h3 class="history">Brief History</h3>
 				<p><?php echo esc_html( CFS()->get( 'brief_history' ) ); ?></p>
 			</div>
 		</section>
 		<section class="leadership" id="leadership">
+				<header class="section-heading">
+					<h2>Leadership</h2>
+				</header>
 			<div class="content-wrapper">
-				<h2>Leadership</h2>
 				<h4>The UNYA Board exmplifies the leadership of our youth.</h4>
 				<ul>
 					<?php foreach ( $leaders as $post ) : setup_postdata( $post ); ?>
@@ -54,15 +62,16 @@ get_header(); ?>
 								<p><?php echo esc_html( CFS()->get( 'bio' ) ); ?></p>
 							</div>
 						</li>
-					<?php endforeach; wp_reset_postdata(); ?> 
-				
+					<?php endforeach; wp_reset_postdata(); ?>
 				</ul>
 				
 			</div>
 		</section>
 		<section class="reports" id="reports">
+				<header class="section-heading">
+					<h2>Reports</h2>
+				</header>
 			<div class="content-wrapper">
-				<h2>Reports</h2>
 				<?php $reports = CFS()->get( 'reports' ); ?>
 				<ul>
 					<?php foreach ( $reports as $report ): ?>
@@ -78,16 +87,17 @@ get_header(); ?>
 				</ul>
 			</div>
 		</section>
+		<section>
+				<header class="section-heading">
+					<h2>Find Us</h2>
+				</header>
+		</section>
+
 		<div class="pre-footer">
 			<p class="pre-footer-text">Like what you see?</p>
 			<p class="pre-footer-text">Support us to help us grow.</p>
 			<button class="donate">Donate Today</button>
 		</div>
-
-
-
-
-
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
