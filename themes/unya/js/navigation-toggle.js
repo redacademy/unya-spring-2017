6ec422b320825af223fa5e472a34d34c810db859
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-15 15:30:25 -0700

    add toggle class on site header on menu-toggle click

--- themes/unya/js/navigation-toggle.js
+++ themes/unya/js/navigation-toggle.js
@@ -1,1 +1,8 @@
-console.log('working');
+(function($) {
+  $(function () {
+    $('.menu-toggle').on('click', function(event) {
+      event.preventDefault();
+      $('.site-header').toggleClass('expanded');
+    })
+  });
+})(jQuery);
