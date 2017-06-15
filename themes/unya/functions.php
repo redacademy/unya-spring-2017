8abd54c4b7483084ee7a25ad329f4596d1071a19
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-15 15:19:24 -0700

    add and enqueue navigation script, style navigation

--- themes/unya/functions.php
+++ themes/unya/functions.php
@@ -86,6 +86,9 @@ function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	wp_enqueue_script( 'font-awesome-cdn','https://use.fontawesome.com/828c02da95.js');
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'navigation-toggle', get_template_directory_uri() . '/js/navigation-toggle.js' , array('jquery'), false, true );
+
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
