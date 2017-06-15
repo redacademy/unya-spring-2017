66d67fd32fb4d81ff8e0eaa52f6633e52abf1288
Jennifer Ta <jenniferta90@gmail.com>
2017-06-15 15:28:38 -0700

    updated header typography and added learn more icon

--- themes/unya/front-page.php
+++ themes/unya/front-page.php
@@ -10,42 +10,45 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+    <h1>UNYA</h1>
+
     <section class="about">
       <div class="content-wrapper">
-        <h1>UNYA: Where youth lead </h1>
+        <h2>UNYA: Where youth lead </h2>
         <p>The talented youth at UNYA are making an impact in our community. Learn about their successes.</p>
-        <h2>Learn more</h2>
+        <h3>Learn more <span class="fa fa-angle-right fa-2x" aria-hidden="true"></span></h3>
       </div>
     </section>
     <section class="nyc">
       <div class="content-wrapper">
-        <h1>Join us in making history</h1>
+        <h2>Join us in making history</h2>
         <p>The Native Youth Center capital campaign will help us build a new hub for
         Vancouver's indigenous youth. Learn how you can contribute.</p>
-        <h2>Learn More</h2>
+        <h3>Learn More <span class="fa fa-angle-right fa-2x" aria-hidden="true"></span></h3>
       </div>
     </section>
     <section class="programs">
       <div class="content-wrapper">
-        <h1>Programs at UNYA</h1>
+        <h2>Programs at UNYA</h2>
         <p>What's your thing? At UNYA, there's something for everyone. Connect with us to
         learn more about the programs that UNYA can offer to you.</p>
-        <h2>Learn More</h2>
+        <h3>Learn More <span class="fa fa-angle-right fa-2x" aria-hidden="true"></span></h3>
       </div>
     </section>
     <section class="donation-cta">
       <div class="content-wrapper">
-        <h1>Make your donation online today</h1>
-        <h1>Donate Today </h1>
+        <h2>Make your donation online today</h2>
+        <h3>Donate Today </h3>
       </div>
     </section>
     <section class="instagram-images">
       <div class="content-wrapper">
-        <h1>Get social with #UNYAwhat</h1> 
-        <?php echo do_shortcode("[instagram-feed num=6 cols=2]");?>
+
+        <h2>Get social with #UNYAwhat</h2>
+        <?php echo do_shortcode("[instagram-feed num=6 cols=2]"); ?>
 
         <!--<?php get_post(); ?>-->
-        
+
       </div>
     </section>
 <?php get_footer(); ?>
