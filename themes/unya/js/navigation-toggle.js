a9d48e89524abab1336a8323f13c73da9cfb16a5
George Tettey <selikem@Georges-MacBook-Pro.local>
2017-06-16 14:57:01 -0700

    style accordion on NYC page

--- themes/unya/js/navigation-toggle.js
+++ themes/unya/js/navigation-toggle.js
@@ -1,13 +1,19 @@
 (function($) {
   $(function () {
     if (window.innerWidth <= 600) {
-      $('#accordion').accordion({heightStyle: 'content'});
+      $('#accordion').accordion({
+        heightStyle: 'content',
+        icons: { 'header': 'ui-icon-triangle-1-s', 'activeHeader': 'ui-icon-caret-1-s' }
+    });
     } 
     $(window).resize(function() {
       if (window.innerWidth > 600) {
         $('#accordion').accordion('destroy');
       } else if (window.innerWidth <= 600) {
-        $('#accordion').accordion({heightStyle: 'content'});
+        $('#accordion').accordion({
+          heightStyle: 'content',
+          icons: { 'header': 'ui-icon-caret-1-s', 'activeHeader': 'ui-icon-caret-1-n' }
+        });
 
       }
     });
