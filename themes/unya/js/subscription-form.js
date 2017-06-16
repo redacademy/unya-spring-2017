3529cf680769009d6653b92b88f2155dfa068586
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-16 16:52:38 -0700

    compile for deploy

--- themes/unya/js/subscription-form.js
+++ themes/unya/js/subscription-form.js
@@ -1,9 +1,9 @@
 (function($) {
   $(function () {
 
-    $btn = $('.sign-up-btn');
+    var $btn = $('.sign-up-btn');
 
-    $btn.on(click, function() {
+    $btn.on('click', function() {
       $btn.toggleClass('hidden-mobile');
       $('subscription-form').removeClass('hidden-mobile').addClass('display-form');
     });
