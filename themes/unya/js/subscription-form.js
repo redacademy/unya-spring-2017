e5681ccb54b4495e1d8a085d63fefa4fbeb45c03
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-17 11:31:16 -0700

    remove form submission message, add change submit btn text on submission

--- themes/unya/js/subscription-form.js
+++ themes/unya/js/subscription-form.js
@@ -2,21 +2,30 @@
   $(function () {
 
     var $btn = $('.sign-up-btn');
-    var $form = $('.subscription-form');
+    var $subscribeArea = $('.subscription-form');
+    var $form = $('.wpcf7-submit');
 
+    // displays form when sign up button clicked
     $btn.on('click', function(event) {
       event.preventDefault();
       $btn.css('display', 'none');
       $('.newsletter-sign-up').css('position', 'absolute');
-      $form.removeClass('hidden-mobile').addClass('display-form');
+      $subscribeArea.removeClass('hidden-mobile').addClass('display-form');
       $('.close-form').prepend('<i class="fa fa-times" aria-hidden="true"></i>');
     });
 
+    // hides form when close button is clicked
     $('.close-form').on('click', function() {
       $btn.css('display', 'initial');
       $('.newsletter-sign-up').css('position', 'static');
-      $form.removeClass('display-form').addClass('hidden-mobile');
+      $subscribeArea.removeClass('display-form').addClass('hidden-mobile');
       $('.close-form').empty();
+      $form.prop('value', 'sign up');
+    });
+
+    // changes button message on submit if successful
+    $form.on('click', function() {
+      $form.prop('value', 'signed up');
     });
 
   });
