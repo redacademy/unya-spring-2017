963ed8fff71ea5dc7d510ffbeb04d083c1bc2b73
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-18 10:30:27 -0700

    order content on single program page, correct color for heading

--- themes/unya/template-parts/program-single.php
+++ themes/unya/template-parts/program-single.php
@@ -10,45 +10,44 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php the_title( '<h2 class="program-title">', '</h2>' ); ?>
 
-	<div class="program-content">
+	<div class="program-content content-wrapper">
 		<p><?php echo wpautop( esc_html( CFS()->get( 'program_details' ) ) ); ?></p>
 		<a href="<?php echo esc_html( CFS()->get( 'program_brochure' ) ); ?>"><i class="fa fa-file-o" aria-hidden="true"></i> <?php the_title(); ?> Program Brochure</a>
+	</div>
 
-		<div class="program-body">
-			<!-- content to be reordered in desktop -->
-			<div class="program-activities">
-				<h3>Program Activities</h3>
-				<ul>
-				<?php	
-					$activities = CFS()->get( 'program_activities' );
-					foreach ( $activities as $activity ) : ?>
-					<li><?php echo $activity['program_activity']; ?></li>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-
-			<?php $images = CFS()->get( 'program_image_slider' ); ?>
-			<?php // to only grab the first 2 images
-				$i = 0;
-				foreach ( $images as $image ) : 
-					if ($i == 3) break;
-			?>
-			<div class="program-image"><img src="<?php echo $image['image']; ?>"></div>
-					<?php $i++;
-				endforeach; ?>
-
-			<div class="program-involvement">
-				<h3>How To Get Involved</h3>
-				<p class="content-wrapper"><?php echo wpautop( esc_html( CFS()->get( 'get_involved' ) ) ); ?></p>
-			</div>
+	<!-- content to be reordered in desktop -->
+	<div class="program-body">
+		<div class="program-activities">
+			<h3>Program Activities</h3>
+			<ul>
+			<?php	
+				$activities = CFS()->get( 'program_activities' );
+				foreach ( $activities as $activity ) : ?>
+				<li><?php echo $activity['program_activity']; ?></li>
+				<?php endforeach; ?>
+			</ul>
 		</div>
 
-		<ul class="image-slider">
-			<?php foreach ( $images as $image ) : ?>
-			<li><img src="<?php echo $image['image']; ?>"></li>
-			<?php endforeach; ?>
-		</ul><!--image slider-->
+		<?php $images = CFS()->get( 'program_image_slider' ); ?>
+		<?php // to only grab the first 2 images
+			$i = 0;
+			foreach ( $images as $image ) : 
+				if ($i == 3) break;
+		?>
+		<div class="program-image"><img src="<?php echo $image['image']; ?>"></div>
+		<?php $i++;
+			endforeach; ?>
 
-	</div><!-- .program-content -->
+		<div class="program-involvement">
+			<h3>How To Get Involved</h3>
+			<p class="content-wrapper"><?php echo wpautop( esc_html( CFS()->get( 'get_involved' ) ) ); ?></p>
+		</div>
+	</div>
+
+	<ul class="image-slider hidden-mobile">
+		<?php foreach ( $images as $image ) : ?>
+		<li><img src="<?php echo $image['image']; ?>"></li>
+		<?php endforeach; ?>
+	</ul><!--image slider-->
 
 </article><!-- #post-## -->
