942cc09b395193011d2b46bb2a3c79bf4139020b
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-18 12:02:36 -0700

    add flex to news-single

--- themes/unya/archive-news.php
+++ themes/unya/archive-news.php
@@ -16,29 +16,33 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-      <header class="page-header">
+			<header class="page-header">
+					<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
 			</header><!-- .page-header -->
 
 		<?php /* Start the Loop */ ?>
 			<section class="news" id="news">
-		 		<div class=“content-wrapper”>
-		 			<ul class="news-articles">
-					 	<?php foreach ( $news as $post ) : setup_postdata( $post ); ?>
-						 	<li class="news-single">
-								<h3><?php the_title();?>
-								</h3>
-								<img class="news-image" src="<?php echo CFS()->get( 'article_photo' ); ?>">			
-								<p><?php echo CFS()->get( 'article_text' ); ?></p>				
-								<p><?php echo CFS()->get( 'article_date' ); ?></p>		
-								<p><?php echo CFS()->get( 'article_url' ); ?></p>						
-		  				</li>
-						<?php endforeach; wp_reset_postdata(); ?> 
-					</ul>
-			  </div>
-      </section>
+				<h2>News</h2>
+				<ul class="news-articles">
+					<?php foreach ( $news as $post ) : setup_postdata( $post ); ?>
+						<li class="news-single">
+							<img class="news-image" src="<?php echo CFS()->get( 'article_photo' ); ?>">
+							<div class="news-details">
+								<h3><?php the_title();?></h3>
+								<p><?php echo CFS()->get( 'article_text' ); ?></p>
+								<p class="hidden-mobile"><?php echo CFS()->get( 'article_date' ); ?></p>
+								<p><?php echo CFS()->get( 'article_url' ); ?></p>
+							</div>
+						</li>
+					<?php endforeach; wp_reset_postdata(); ?> 
+				</ul>
+			</section>
  
-      <section class="media-inquiries" id="media-queries">
-      </section>
+			<section class="media-inquiries" id="media-queries">
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
