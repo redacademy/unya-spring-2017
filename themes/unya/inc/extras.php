adceceaa0f3f57214eec900e36b4270b629c842d
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-20 17:08:54 -0700

    test sidebar nav menu

--- themes/unya/inc/extras.php
+++ themes/unya/inc/extras.php
@@ -125,3 +125,33 @@ function hero_banners() {
     wp_add_inline_style( 'custom-style', $custom_css );
 }
 add_action( 'wp_enqueue_scripts', 'hero_banners' );
+
+// create list of sub-menu items
+/* Returns all child nav_menu_items under a specific parent
+*
+* @param int the parent nav_menu_item ID
+* @param array nav_menu_items
+* @param bool gives all children or direct children only
+* @return array returns filtered array of nav_menu_items
+*/
+
+// function get_nav_menu_item_children( $parent_id, $nav_menu_items, $depth = true ) {
+//     $nav_menu_item_list = array();
+    
+//     foreach ( (array) $nav_menu_items as $nav_menu_item ) {
+//         if ( $nav_menu_item->menu_item_parent == $parent_id ) {
+//             $nav_menu_item_list[] = $nav_menu_item;
+//             if ( $depth ) {
+//                 if ( $children = get_nav_menu_item_children( $nav_menu_item->ID, $nav_menu_items ) )
+//                 $nav_menu_item_list = array_merge( $nav_menu_item_list, $children );
+//             }
+//         }
+//     }
+//     return $nav_menu_item_list;
+// }
+
+// if ( is_active_sidebar( 'sidebar-1' ) ) {
+//     echo '<pre>';
+//     print_r($nav_menu_item_list);
+//     echo '</pre>';
+// }
--- themes/unya/sidebar.php
+++ themes/unya/sidebar.php
@@ -5,11 +5,31 @@
  * @package UNYA_Theme
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
+// if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+// 	return;
+// }
+
+function get_nav_menu_item_children( $parent_id, $nav_menu_items, $depth = true ) {
+    $nav_menu_item_list = array();
+    
+    foreach ( (array) $nav_menu_items as $nav_menu_item ) {
+        if ( $nav_menu_item->menu_item_parent == $parent_id ) {
+            $nav_menu_item_list[] = $nav_menu_item;
+            if ( $depth ) {
+                if ( $children = get_nav_menu_item_children( $nav_menu_item->ID, $nav_menu_items ) )
+                $nav_menu_item_list = array_merge( $nav_menu_item_list, $children );
+            }
+        }
+    }
+    return $nav_menu_item_list;
 }
+
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php
+		echo '<pre>';
+		print_r($nav_menu_item_list);
+		echo '</pre>';
+	?>
 </div><!-- #secondary -->
