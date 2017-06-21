e741a5200a9f303a0b379bb5846541debf19d139
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-21 16:12:05 -0700

    create function to fix sidebar on page

--- themes/unya/js/sidebar.js
+++ themes/unya/js/sidebar.js
@@ -1,7 +1,27 @@
 (function($) {
   $(function () {
 
-    
+    // sidebar positioning
+    var $bottomOfHeader = $('.header-wrapper').offset().top + $('.header-wrapper').height();
+    var $sidebarMenu = $('#secondary .menu-primary-menu-container');
+    var $sidebarArea = $('.sidebar-nav-menu');
+
+    function fixSidebar(position) {
+      if (position > $bottomOfHeader) {
+        $sidebarMenu.addClass('fixed-sidebar');
+      } else {
+        $sidebarMenu.removeClass('fixed-sidebar');
+      }
+    }
+
+    $(document).scroll(function() {
+      var $position = $(this).scrollTop();
+      
+      if ($('.sidebar-nav-menu').length) {
+        fixSidebar($position);
+      }
+
+    });
 
     });
 
