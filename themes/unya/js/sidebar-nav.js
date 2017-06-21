59770037f806af56f9bdf1161cbf5399443cd4d2
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-21 16:46:43 -0700

    add style to sidebar, add more specific to page-templates.scss

--- themes/unya/js/sidebar-nav.js
+++ themes/unya/js/sidebar-nav.js
@@ -6,6 +6,8 @@
     var $sidebarMenu = $('#secondary .menu-primary-menu-container');
     var $sidebarArea = $('.sidebar-nav-menu');
 
+    $sidebarMenu.css('top', $bottomOfHeader);
+
     function fixSidebar(position) {
       if (position > $bottomOfHeader) {
         $sidebarMenu.addClass('fixed-sidebar');
