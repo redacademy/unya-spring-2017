a7471868640fbdd29d322ebb10a916ad1310b55b
George Tettey <selikem@Georges-MBP.hitronhub.home>
2017-06-21 13:59:00 -0700

    add smooth scrolling to current page nav submenu

--- themes/unya/js/navigation-toggle.js
+++ themes/unya/js/navigation-toggle.js
@@ -35,7 +35,19 @@
 
       });
     }
-    
+  
+    $('.current-menu-item .sub-menu .menu-item a').click(function(event) {
+      event.preventDefault();
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+      
+    });
     
 
 })(jQuery);
