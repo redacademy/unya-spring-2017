d41375fa2692b7a40acb13ca4e4a0efc3eb03bca
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-22 11:07:59 -0700

    add smooth scrolling to sidebar nav

--- themes/unya/js/sidebar-nav.js
+++ themes/unya/js/sidebar-nav.js
@@ -1,11 +1,11 @@
 (function($) {
   $(function () {
 
-    // sidebar positioning
+    // sidebar positioning on scroll
     var $bottomOfHeader = $('.header-wrapper').offset().top + $('.header-wrapper').height();
     var $sidebarMenu = $('#secondary .menu-primary-menu-container');
     var $sidebarArea = $('.sidebar-nav-menu');
-    
+    var $menuItem = $('.sidebar-menu-container .menu-item a');
 
     $sidebarMenu.css('top', $bottomOfHeader);
 
@@ -21,10 +21,25 @@
 
     $(document).scroll(function() {
       var $position = $(this).scrollTop();
-      console.log($position);
 
       if ($sidebarArea.length) {
         fixSidebar($position);
+      }
+
+    });
+
+    // Smooth scrolling to anchors on the page
+    $menuItem.on('click', function(event) {
+      event.preventDefault();
+
+      var $menuLink = $(this.hash);
+      $menuItem = $menuLink.length ? $menuLink : $('[name=' + this.hash.slice(1) +']');
+      
+      if ($menuItem.length) {
+        $('html, body').animate({
+          scrollTop: $menuLink.offset().top
+        }, 500);
+        return false;
       }
 
     });
