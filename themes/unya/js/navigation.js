d836e11d721d61f1e8c01a866d27c00442d96dba
selikem <selikem@gmail.com>
2017-06-22 14:48:43 -0700

    fix console errors

--- themes/unya/js/navigation.js
+++ themes/unya/js/navigation.js
@@ -7,9 +7,9 @@
     });
     } 
     $(window).resize(function() {
-      if (window.innerWidth > 600) {
+      if (window.innerWidth === 600) {
         $('#accordion').accordion('destroy');
-      } else if (window.innerWidth <= 600) {
+      } else if (window.innerWidth < 600) {
         $('#accordion').accordion({
           heightStyle: 'content',
           icons: { 'header': 'ui-icon-caret-1-s', 'activeHeader': 'ui-icon-caret-1-n' }
