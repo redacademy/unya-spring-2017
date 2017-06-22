c9527c02cc56f594e808313f63e85835072ed5c5
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-21 20:05:25 -0700

    fix function for sidebar scroll

--- themes/unya/js/sidebar-nav.js
+++ themes/unya/js/sidebar-nav.js
@@ -5,26 +5,30 @@
     var $bottomOfHeader = $('.header-wrapper').offset().top + $('.header-wrapper').height();
     var $sidebarMenu = $('#secondary .menu-primary-menu-container');
     var $sidebarArea = $('.sidebar-nav-menu');
+    
 
     $sidebarMenu.css('top', $bottomOfHeader);
 
     function fixSidebar(position) {
       if (position > $bottomOfHeader) {
-        $sidebarMenu.addClass('fixed-sidebar');
+        $sidebarMenu.css('position', 'fixed');
+        $sidebarMenu.css('top', '0');
       } else {
-        $sidebarMenu.removeClass('fixed-sidebar');
+        $sidebarMenu.css('position', 'absolute');
+        $sidebarMenu.css('top', $bottomOfHeader);
       }
     }
 
     $(document).scroll(function() {
       var $position = $(this).scrollTop();
-      
+      console.log($position);
+
       if ($sidebarArea.length) {
         fixSidebar($position);
       }
 
     });
 
-    });
+  });
 
 })(jQuery);
