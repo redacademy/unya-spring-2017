53b5db89a9e7995500fbe50d6427508498c5c239
Cory <Coryjh85@gmail.com>
2017-06-23 10:33:21 -0700

    news page layout

--- themes/unya/archive-news.php
+++ themes/unya/archive-news.php
@@ -9,19 +9,12 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-<<<<<<< HEAD
-			<header class="header-wrapper news-title sidebar-start">
-					<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-=======
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
 					get_template_part( 'template-parts/content-news' );
->>>>>>> switched news page back to original loop
+
 				?>
 
 			<?php endwhile; ?>
