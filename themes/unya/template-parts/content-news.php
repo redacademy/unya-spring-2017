1c6f63064103e35a52c16bfda7b3152ab678d868
Cory <Coryjh85@gmail.com>
2017-06-23 10:32:46 -0700

    news template

---
+++ themes/unya/template-parts/content-news.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package RED_Starter_Theme
+ */
+ $args= array(
+     'post_type'=> 'news',
+     'posts_per_page'=> 8
+ );
+
+ $news = get_posts($args);
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+	</header><!-- .entry-header -->
+  	
+						<li class="news-single">
+							<img class="news-image" src="<?php echo CFS()->get( 'article_photo' ); ?>">
+							<div class="news-details">
+								<h3><?php the_title();?></h3>
+								<p><?php echo CFS()->get( 'article_text' ); ?></p>
+								<p class="hidden-mobile"><?php echo CFS()->get( 'article_date' ); ?></p>
+								<p><?php echo CFS()->get( 'article_url' ); ?></p>
+							</div>
+						</li>
+				
+</article><!-- #post-## -->
