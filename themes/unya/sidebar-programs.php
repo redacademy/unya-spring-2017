0c0a790b4531a886c1f6a4f23cb0d0098db15dd2
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-23 14:46:04 -0700

    create program sidebar

--- themes/unya/sidebar-programs.php
+++ themes/unya/sidebar-programs.php
@@ -8,14 +8,34 @@
 ?>
 
 <div id="secondary" class="widget-area hidden-mobile sidebar-nav-menu" role="complementary">
-	<?php
+	<div class="menu-primary-menu-container">
+		<ul class="sidebar-menu-container">
+		<?php
 
-		wp_nav_menu( array( 
-			'theme_location' => 'primary', 
-			'sub_menu' => true, 
-			'menu_class'=>'sidebar-menu-container', 
-			'menu_id'=>'sidebar-menu' ) 
-		);
+		$term = get_query_var( 'program-type' );
+		echo $term;
 
-	?>
+			$program_types = get_terms(array (
+				'taxonomy'  => 'program-type',
+				'hide_empty'=> false
+				));
+				// $terms = wp_get_post_terms( $post->ID, 'program-type');
+				// $term = $terms[0];
+				// echo '<li class="menu-item-archive menu-success-title"><p>' . print_r($term->name) .'</p></li>';
+			if ( !empty($program_types) && !is_wp_error($program_types) ):
+		?>
+		<?php foreach ($program_types as $program_type):?>
+			<li class="menu-item-archive success-items">
+				<a href="<?php echo get_term_link($program_types) ?>">
+				<p><?php echo $program_types->name; ?></p>
+				</a>
+			</li>
+		<?php endforeach; ?>
+		<?php endif;
+
+		// echo '<li class="menu-item-archive"><a href="' . get_site_url() . '/impact/#statistics">Statistics</a></li>';
+		// echo '<li class="menu-item-archive"><a href="' . get_site_url() . '/impact/#testimonials">Testimonials</a></li>'; ?>
+	
+		</ul>
+	</div>
 </div><!-- #secondary -->
--- themes/unya/single-program.php
+++ themes/unya/single-program.php
@@ -27,5 +27,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php get_sidebar('programs'); ?>
 <?php get_footer(); ?>
