a2845d12509d77205c1ab6acc0953f6145ac41dd
Jessica Dieuique <jessica.dieuique@gmail.com>
2017-06-24 13:59:12 -0700

    update layout of single programs, remove 1 image

--- themes/unya/template-parts/program-single.php
+++ themes/unya/template-parts/program-single.php
@@ -14,7 +14,10 @@
 		<a href="<?php echo esc_html( CFS()->get( 'program_brochure' ) ); ?>"><i class="fa fa-file-o" aria-hidden="true"></i> <?php the_title(); ?> Program Brochure (PDF)</a>
 	</div>
 
-	<!-- content to be reordered in desktop -->
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="program-image"><?php the_post_thumbnail( 'large' ); ?></div>
+	<?php endif; ?>
+
 	<div class="program-body">
 		<div class="program-activities content-wrapper">
 			<h3>Program Activities</h3>
@@ -27,16 +30,6 @@
 			</ul>
 		</div>
 
-		<?php $program_images = CFS()->get( 'program_image_slider' ); ?>
-		<?php // to only grab the first 2 images
-			$i = 0;
-			foreach ( $program_images as $program_image ) : 
-				if ($i == 3) break;
-		?>
-		<div class="program-image"><img src="<?php echo $program_image['program_image']; ?>"></div>
-		<?php $i++;
-			endforeach; ?>
-
 		<div class="program-involvement content-wrapper">
 			<h3>How To Get Involved</h3>
 			<p class="content-wrapper"><?php echo wpautop( esc_html( CFS()->get( 'get_involved' ) ) ); ?></p>
@@ -44,7 +37,6 @@
 	</div>
 
 	<div class="image-carousel hidden-mobile">
-
     <?php $program_images = CFS()->get( 'program_image_slider' ); ?>
     <?php foreach ( $program_images as $program_image ) : ?>
 	  <div style='background-size: cover;
