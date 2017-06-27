7c9e037a4e99975c6fc7b97f00aef29856e2ee9c
Jennifer Ta <jenniferta90@gmail.com>
2017-06-27 13:20:09 -0700

    desktop prefooter for impact page css edits

--- themes/unya/template-parts/prefooter-fit.php
+++ themes/unya/template-parts/prefooter-fit.php
@@ -1,4 +1,4 @@
-	<section class="pre-footer fit">
-		<p>Inspired by what you see? Find your fit at UNYA.</p>
-		<h2><a href="#">Find Your Fit</a></h2>
+	<section class="pre-footer fit-button">
+		<p>Inspired by what you see? Check out our programs to find your fit at UNYA.</p>
+		<h2><a href="<?php echo get_post_type_archive_link( 'program' ) ?>">Find Your Fit</a></h2>
 	</section>
