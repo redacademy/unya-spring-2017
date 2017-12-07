108be14f97bb85cf5214ed6ec6164a2b100302f8
Cory <Coryjh85@gmail.com>
2017-12-07 13:48:12 -0800

    messing w accordions

--- themes/unya/page-impact.php
+++ themes/unya/page-impact.php
@@ -4,18 +4,7 @@
  *
  * @package UNYA_Theme
  */
- 
- $args= array(
-     'post_type'=> 'success_story',
-     'posts_per_page'=> 6
- );
- $success_story = get_posts($args);
- 
- $args= array(
-     'post_type'=> 'volunteer',
-     'posts_per_page'=> 6
- );
- $volunteer = get_posts($args);
+
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -33,17 +22,22 @@ get_header(); ?>
 		</section>
 
 		<div class="accordion" id="accordion">
-		
+		<?php /* Start the Loop */ ?>
 		<h2 class="accordion-label" id="success-stories">Success Stories</h2>
 	  <section class="success-stories">
 			<header class="section-heading hidden-mobile">
 				<h2 class="hidden-mobile">Success Stories</h2>
 				<h4 class="hidden-mobile">Learn more about the talented youth at UNYA, and the successes we celebrate.</h4>
 			</header>
+			
 			<div class="content-wrapper">
 				<h4 class="hidden-desktop">Learn more about the talented youth at UNYA, and the successes we celebrate.</h4>
-			
-				<?php foreach ( $success_story as $post ) : setup_postdata( $post ); ?>
+		  <?php  $args= array(
+       'post_type'=> 'success_story',
+       'posts_per_page'=> 6
+        );
+          $success_story = get_posts($args);?>
+			<?php foreach ( $success_story as $post ) : setup_postdata( $post ); ?>
 				  <section class="single-story hidden-mobile">
 				  	<a class="single-story-mobile" href="<?php the_permalink() ?>">
               <div class="hidden-desktop success-image" style="background-image: linear-gradient(180deg, rgba(87,87,87,0) 0%, rgba(67,67,67,0.4) 52.31%, rgba(44,44,44,0.7) 100%),
@@ -64,67 +58,67 @@ get_header(); ?>
 				    </div>
 				  </section>
 		    <?php endforeach; wp_reset_postdata(); ?>
-      </div>
-    </section>
+    </div>
+	</section>
 
 
+	<h2 class="accordion-label" id="volunteers">Volunteers</h2>
+	<section class="volunteers">
+		<header class="section-heading hidden-mobile">
+			<h2 class="hidden-mobile">Volunteers</h2>
+			<h4 class="hidden-mobile">Learn more about some of our hardworking volunteers.</h4>
+		</header>
+		<h4 class="hidden-desktop">Learn more about some of our hardworking volunteers.</h4>
 	
-		<h2 class="accordion-label" id="volunteer">Volunteers</h2>
-	  <section class="volunteers">
-			<header class="section-heading content-wrapper hidden-mobile">
-				<h2 class="hidden-mobile">Volunteers</h2>
-				<h4 class="hidden-mobile">Learn more about some of our hardworking volunteers.</h4>
-			</header>
-			
-			<div class="content-wrapper">
-				<h4 class="hidden-desktop">Learn more about some of our hardworking volunteers.</h4>
-		   	<?php foreach ( $volunteer as $post ) : setup_postdata( $post ); ?>
+<div class="content-wrapper">
+  <?php $args= array(
+     'post_type'=> 'volunteer',
+     'posts_per_page'=> 6
+      );
+   $volunteer = get_posts($args);?>
+	  
+<?php foreach ( $volunteer as $post ) : setup_postdata( $post ); ?>
 				  <section class="single-story hidden-mobile">
-				   	<a class="single-story-mobile" href="<?php the_permalink() ?>">
-					  	<div class="hidden-desktop success-image" style="background-image: linear-gradient(180deg, rgba(87,87,87,0) 0%, rgba(67,67,67,0.4) 52.31%, rgba(44,44,44,0.7) 100%),
-							  url(<?php echo CFS()->get( 'photo' ); ?>)";>
-							<h3><?php the_title() ?></h3>
-						  </div>
+				  	<a class="single-story-mobile" href="<?php the_permalink() ?>">
+              <div class="hidden-desktop success-image" style="background-image: linear-gradient(180deg, rgba(87,87,87,0) 0%, rgba(67,67,67,0.4) 52.31%, rgba(44,44,44,0.7) 100%),
+						  url(<?php echo CFS()->get( 'photo' ); ?>)";>
+                <h3><?php the_title() ?></h3>
+					  	</div>
 					  </a>
-					<a href="<?php the_permalink() ?>"><img class="success-image hidden-mobile" src="<?php echo CFS()->get( 'photo' ); ?>" cover>
-					</a>
-					<div class="success-content">
-						<h3 class="hidden-mobile">Youth Feature - <?php the_title(); ?></h3>
-						<p class="hidden-mobile"><?php echo CFS()->get( 'first_half_volunteer_story' ); ?>
-						</p>
-						<p class="hidden-mobile">
-							<a class="hidden-mobile impact-read-more" href="<?php the_permalink() ?>">Read more about <?php the_title(); ?>
-								<i class="fa fa-arrow-right" aria-hidden="true"></i>
-							</a>
-						</p>				
-					</div>
-				</section>
-		  <?php endforeach; wp_reset_postdata(); ?>
-		</div>	
-  </section>
-   
+					  <a href="<?php the_permalink() ?>"><img class="success-image hidden-mobile" src="<?php echo CFS()->get( 'photo' ); ?>" cover>
+					  </a>
+					  <div class="success-content">
+						  <h3 class="hidden-mobile">Youth Feature - <?php the_title(); ?></h3>
+						  <p class="hidden-mobile"><?php echo CFS()->get( 'first_half_volunteer_story' ); ?></p>
+							<p class="hidden-mobile">
+								<a class="hidden-mobile impact-read-more" href="<?php the_permalink() ?>">Read more about <?php the_title(); ?>
+									<i class="fa fa-arrow-right" aria-hidden="true"></i>
+								</a>
+							</p>  			
+				    </div>
+				  </section>
+		    <?php endforeach; wp_reset_postdata(); ?>
+				</div>
+	</section>
 
 		<section class="statistics" id="statistics">
-			<h2 class="impact-head content-wrapper">Impacts</h2>
-      <div class="image-carousel content-wrapper">
+			<div class="image-carousel content-wrapper">
         <?php $stats_slider = CFS()->get( 'statistics_slider' ); ?>
         <?php foreach ( $stats_slider as $stats_slide ) : ?>
  	        <div class="stats-slide" style="background-image: url(<?php echo $stats_slide['stats_image']; ?>)";>
  		      </div>
  		    <?php endforeach; ?>
       </div>	     
-	  </section>
+		</section>
 
 		<section class="testimonials" id="testimonials">
 			<h2 class="content-wrapper">Testimonials</h2>
 			<p class="content-wrapper testimonial-body">What do youth say about us?</p>
+
 			<div class="impact-polygon">
 			  <p class="content-wrapper"><?php echo wp_kses(CFS()->get( 'testimonial_1' ),array('br')); ?></p>
 			</div>
-  		<div class="rectangle-container">	
-			  <p class="content-wrapper"><?php echo CFS()->get( 'testimonial_2' ); ?>
-				</p>
-			</div>
+  		<div class="rectangle-container">	<p class="content-wrapper"><?php echo CFS()->get( 'testimonial_2' ); ?></p></div>
 		</section>
 
 		<?php get_template_part( 'template-parts/prefooter', 'fit' ); ?>
