825805ed11f8a616f6385fff918301b592aa6571
tmclean85 <trevor.a.mclean@gmail.com>
2017-12-30 19:12:05 -0800

    Adjust search not found page styling
    Change return link href

--- themes/unya/sidebar.php
+++ themes/unya/sidebar.php
@@ -9,13 +9,11 @@
 
 <div id="secondary" class="widget-area desktop-only sidebar-nav-menu" role="complementary">
 	<?php
-
 		wp_nav_menu( array( 
 			'theme_location' => 'primary',
 			'sub_menu' => true, 
 			'menu_class'=>'sidebar-menu-container', 
 			'menu_id'=>'sidebar-menu' ) 
 		);
-
 	?>
 </div><!-- #secondary -->
