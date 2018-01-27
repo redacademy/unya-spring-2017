a15faa17e46213738e57c38ffc311bec75ef4460
tmclean85 <trevor.a.mclean@gmail.com>
2018-01-26 16:02:24 -0800

    Minor styling bug squashes to NYC page

--- themes/unya/template-parts/prefooter-donation.php
+++ themes/unya/template-parts/prefooter-donation.php
@@ -1,5 +1,5 @@
 
 	<section class="pre-footer donation-button sidebar-stop">
 		<p>Like what you see? <span class="line-break">Support us to help us grow.</span></p>
-		<h2><a href="https://www.canadahelps.org/en/charities/urban-native-youth-association/">Donate <span class="hidden-mobile">Today</span></a></h2>
+		<h2 class="prefooter-donation-button"><a href="https://www.canadahelps.org/en/charities/urban-native-youth-association/">Donate <span class="hidden-mobile">Today</span></a></h2>
 	</section>
