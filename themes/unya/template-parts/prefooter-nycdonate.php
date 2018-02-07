febdad323c5caad18a8ec968a60c9c513fa4a423
Jessica Dieuique <jessica.dieuique@gmail.com>
2018-02-06 21:24:11 -0800

    mobile styling of nyc

---
+++ themes/unya/template-parts/prefooter-nycdonate.php
@@ -0,0 +1,5 @@
+
+	<section class="pre-footer donation-button sidebar-stop">
+		<p>Support Unya Today</p>
+		<h2 class="prefooter-donation-button"><a href="https://www.canadahelps.org/en/charities/urban-native-youth-association/">Donate <span class="hidden-mobile">Today</span></a></h2>
+	</section>
