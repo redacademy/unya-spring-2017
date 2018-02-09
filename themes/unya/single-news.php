6d1a2674fd700e148bdd3c0e410f39e605cb3494
tmclean85 <trevor.a.mclean@gmail.com>
2018-02-08 21:11:54 -0800

    Remove single news quotations, move single news categories to bottom of page at all widths

--- themes/unya/single-news.php
+++ themes/unya/single-news.php
@@ -14,17 +14,6 @@ get_header(); ?>
 			<a class="back-link news-back" href='news'><i class="fa fa-arrow-left" aria-hidden="true"></i>Back to News</a>
 			<h2 id="single-news-title"><?php the_title(); ?></h2>
 			<p class="single-news-datestamp"><?php echo get_the_date(); ?></p>
-			<div class="single-news-tag-wrapper">
-				<?php
-				  $id = get_the_ID();
-          $tags = wp_get_post_tags($id);
-          if($tags) : foreach ($tags as $tag) :
-						$new_str = str_replace(' ', '+', $tag->name);
-						$root = get_site_url(); ?>
-            <a href="<?php echo $root . '/?s=' . $new_str ?>"><div class="news-tax-term-box single-news-tax"><p class="single-news-tag-title"><?php echo $tag->name ?></p></div></a>
-				<?php endforeach; ?>
-				<?php endif; ?>
-			</div>
 			<div class="single-news-main">
 			  <div class="single-news-wrapper">
 					<p><?php echo CFS()->get('first_half_of_news_story') ?></p>
@@ -41,12 +30,23 @@ get_header(); ?>
 			      	<?php the_post_thumbnail('large'); ?>
 					  </div>
 					  <div class="news-quote-desktop-wrapper">
-					  	<h2><?php echo '"' . $quote . '"' ?></h2>
+					  	<h2><?php echo $quote; ?></h2>
 					  </div>	
 					</span>
 					<p><?php echo CFS()->get('second_half_of_news_story') ?></p>				
 				</div>
-      </div><!--single-news-main-->
+			</div><!--single-news-main-->
+			<div class="single-news-tag-wrapper">
+				<?php
+				  $id = get_the_ID();
+          $tags = wp_get_post_tags($id);
+          if($tags) : foreach ($tags as $tag) :
+						$new_str = str_replace(' ', '+', $tag->name);
+						$root = get_site_url(); ?>
+            <a href="<?php echo $root . '/?s=' . $new_str ?>"><div class="news-tax-term-box single-news-tax"><p class="single-news-tag-title"><?php echo $tag->name ?></p></div></a>
+				<?php endforeach; ?>
+				<?php endif; ?>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_sidebar(); ?>
