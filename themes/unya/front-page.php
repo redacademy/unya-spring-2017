80108d9e298a56bb6bbaeef8940264befa6e4dc2
Jessica Dieuique <jessica.dieuique@gmail.com>
2018-02-20 20:10:33 -0800

    update home page banner and donation text

--- themes/unya/front-page.php
+++ themes/unya/front-page.php
@@ -14,31 +14,31 @@ get_header(); ?>
 
    <section class="about">
      <div class="content-wrapper ">
-       <h2 id="front-page-slider-header">UNYA: Where Youth lead </h2>
-       <p>The talented youth at UNYA are making an impact in our community. Learn about their successes.</p>
-       <a class="homepage-links" href="about"><h3>Learn more <span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>     
+       <h2 id="front-page-slider-header">UNYA: Where Youth lead</h2>
+       <p>The talented youth at Urban Native Youth Association are making an impact in our community. Learn about their successes.</p>
+       <a class="homepage-links" href="about"><h3>Get Inspired<span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>
   	 </div>
    </section>
     <section class="nyc">
       <div class="content-wrapper">
         <h2>Join us in making history</h2>
-        <p>The Native Youth Center capital campaign will help us build a new hub for
-        Vancouver's indigenous youth. Learn how you can contribute.</p>
-        <a class="homepage-links" href="native-youth-center"><h3>Learn More <span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>
+        <p>The Native Youth Centre will be an innovative new hub for Indigenous youth 
+        empowerment. Learn more about our plans for the Native Youth Centre, and how you can contribute.</p>
+        <a class="homepage-links" href="native-youth-center"><h3>Discover our vision<span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>
       </div>
     </section>
     <section class="programs">
       <div class="content-wrapper">
         <h2>Programs at UNYA</h2>
-        <p>What's your thing? At UNYA, there's something for everyone. Connect with us to
-        learn more about the programs that UNYA can offer to you.</p>
-        <a  class="homepage-links" href="<?php echo get_post_type_archive_link( 'program' ) ?>"><h3>Learn More <span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>
+        <p>What’s your thing? At UNYA, there’s something for everyone. Connect with us
+        to learn more about the programs that UNYA offers.</p>
+        <a  class="homepage-links" href="<?php echo get_post_type_archive_link( 'program' ) ?>"><h3>Explore our programs<span class="fa fa-angle-right" aria-hidden="true"></span></h3></a>
       </div>
     </section>
     <section class="donation-call">
       <div class="content-wrapper">
-				<h3 class="capitalize hidden-mobile">Make your donation online today</h3>
-        <a target="_blank" href="https://www.canadahelps.org/en/charities/urban-native-youth-association/">
+				<h3 class="capitalize hidden-mobile">Make a difference for indigenous youth in BC</h3>
+        <a target="_blank" href="https://www.canadahelps.org/en/dn/33910">
 					<h3 class="capitalize donate-button">Donate today</h3>
 				</a>
       </div>
